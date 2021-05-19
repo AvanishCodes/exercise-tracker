043eed0bd62844da9652388db1be93f772ff8127
Avanish Gupta <avanishcodes@gmail.com>
2021-05-19 11:09:30 +0530

    Completed basic setup, enhancements required

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Import from the components
+import Navbar from "./components/navbar.component";
+import ExerciseList from "./components/exercises-list.component";
+import EditExercises from "./components/edit-exercise.component";
+import CreateExercises from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      {/* </Navbar> */}
+      <Route path='/' exact component={ExerciseList} />
+      <Route path='/edit/:id' exact component={EditExercises} />
+      <Route path='/create' exact component={CreateExercises} />
+      <Route path='/user' exact component={CreateUser} />
+    </Router>
   );
 }
 
